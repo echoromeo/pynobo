a3511f6e1689dcbf17e895ecff6879baa97cac73
Carl Petter Levy <cplevy@online.no>
2018-04-23 23:13:02 +0200

    Cleaned up some trailing whitespace

--- nobo.py
+++ nobo.py
@@ -44,17 +44,17 @@ class nobo:
         RESPONSE_UPDATE_HUB_INFO = 'V03'
         RESPONSE_UPDATE_INTERNET_ACCESS = 'V06'
 
-        # Removes a Zone from the hub's internal database. All values except Zone lD is ignored. 
-        # Any Components in the Zone are also deleted (and S02 Component deleted messages arc sent for the deleted Components). 
+        # Removes a Zone from the hub's internal database. All values except Zone lD is ignored.
+        # Any Components in the Zone are also deleted (and S02 Component deleted messages arc sent for the deleted Components).
         # Any Component used as temperature sensor for the Zone is modified to no longer be temperature sensor for the Zone (and a V0l Component updated message is sent).
         # R00 <Zone id> <Name> <Active week profile id> <Comfort temperature> <Eco temperature> <Allow overrides> <Active override id>
         REMOVE_ZONE = 'R00'
-        
+
         # Removes a Component from the hub's internal database. All values except Component Serial Number is ignored.
         # R01 <Serial number> <Status> <Name> <Reverse on/off?> <Zoneid> <Active override Id> <Temperature sensor for zone>
         REMOVE_COMPONENT = 'R01'
 
-        # Removes a WeekProfile from the hub's intemal  database. All values except  Week Profile lD is ignored. 
+        # Removes a WeekProfile from the hub's intemal  database. All values except  Week Profile lD is ignored.
         # Any Zones that are set to use the Week Profile are set to use the default Week Profile in stead (and V00 Zone updated messages are sent).
         # R02 <Week profile id> <Name> <Profile>
         REMOVE_WEEK_PROFILE = 'R02'
@@ -64,25 +64,25 @@ class nobo:
         RESPONSE_REMOVE_WEEK_PROFILE = 'S02'
         RESPONSE_REMOVE_OVERRIDE = 'S03'
 
-        # Gets all information from hub. Will trigger a sequence  of series of one HOO message, zero or more 
-        # HOI, zero or more H02, zero or more Y02, zero or more H03, zero or more H04 commands, one V06 
-        # message if <.:onne<.:ted v ia LAN (not Internet), and lastly a H05 message. 'll1e client  knows 
+        # Gets all information from hub. Will trigger a sequence  of series of one HOO message, zero or more
+        # HOI, zero or more H02, zero or more Y02, zero or more H03, zero or more H04 commands, one V06
+        # message if <.:onne<.:ted v ia LAN (not Internet), and lastly a H05 message. 'll1e client  knows
         # that the Hub is tlnished  sending all info when it has received  the H05 mess3ge.
         GET_ALL_INFO = 'G00'
-        
-        # (Never  used by the Nobo Energy  Control app- you should only use GOO.) Gets all Zones lrom hub. 
+
+        # (Never  used by the Nobo Energy  Control app- you should only use GOO.) Gets all Zones lrom hub.
         # Will trigger a series of H01 messages from the Hub.
         GET_ALL_ZONES = 'G01'
 
-        # (Never  used by the Nobo Energy  Control app - you should only use GOO.) Gets all Components li·om hub. 
+        # (Never  used by the Nobo Energy  Control app - you should only use GOO.) Gets all Components li·om hub.
         # Will result i n a series of H02 messages  from the Hub.
         GET_ALL_COMPONENTS = 'G02'
 
-        # (Never used by the Nobo Energy Control app- you should only use GOO.) Gets all WeekProfile data from hub. 
+        # (Never used by the Nobo Energy Control app- you should only use GOO.) Gets all WeekProfile data from hub.
         # Will trigger a series of 1103 messages from the lluh.
         GET_ALL_WEEK_PROFILES = 'G03'
 
-        # (Never used by the Nobo Energy Control app - you should only use GOO.) Gets all active overrrides from hub. 
+        # (Never used by the Nobo Energy Control app - you should only use GOO.) Gets all active overrrides from hub.
         # Will trigger a series of H04 messages from the Hub.
         GET_ACTIVE_OVERRIDES = 'G04'
 
