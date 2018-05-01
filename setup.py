664689797d5e796b6abfc1646665765f5d67443d
echoromeo <post@undereksponert.no>
2018-05-01 23:36:06 +0200

    Fixing markdown

--- setup.py
+++ setup.py
@@ -41,12 +41,12 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     #
     # pynobo: API version + serial release + alpha/beta/rc 
-    version='1.1.0a1',  # Required
+    version='1.1.0a2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Nobø Hub HVAC Interface',  # Required
+    description='Nobø Hub / Nobø Energy Control Websocket Interface',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
